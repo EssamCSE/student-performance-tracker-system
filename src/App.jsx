196ec38655686bd1708bdbf3fde91279fbbe0db4
Essam <hamaliessam2002@gmail.com>
2025-04-28 14:45:20 +0600

    Set  Up

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button} from "@/components/ui/button"
+import { Toaster } from "@/components/ui/sonner"
+import { toast } from "sonner"
+
+function App() {
+  
+  toast("Event has been created.")
+  
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
